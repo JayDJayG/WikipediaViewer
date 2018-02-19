51a3fa0ba492c0df605d33d1be1a4e650b3298a7
Jay Guzman <j.d.guzman.v@gmail.com>
2018-02-19 09:29:58 -0700

    Added bootsrap 4 style to the App

--- src/App.js
+++ src/App.js
@@ -40,8 +40,8 @@ updateSearch(event){
     const {object} = this.state;
     return(
     <div>
-      <div style= {{display : 'inline-block', width: '15%' }}>
-        <h3>WikiViewer</h3>
+      <div className="text-center" style= {{display : 'inline-block', width: '15%' }}>
+        <h3>WikipediaSearch: </h3>
         <input className="App-input"
         type = "text" value = {this.state.search}
         onChange = {this.updateSearch.bind(this)}/>
@@ -53,7 +53,7 @@ updateSearch(event){
 }
 
 class QueryResolutions extends Component {
-  
+
   render() {
     const wikiobjects = this.props.wikiobject;
 
@@ -67,11 +67,19 @@ class QueryResolutions extends Component {
 
 else {
   return (
-  <div className = "Box">
-  {
+  <div className = "row">
+    <div className = "col-md-12">
+      <div className="card-block mt-4">
+        <div className = "">
 
-  }
+          <h3 className="mb-3">Hola mundo</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In facilisis erat sit amet fermentum suscipit. Maecenas imperdiet nulla quam, ut pellentesque velit ultrices sed. Quisque eleifend condimentum nisi vel egestas. Sed sollicitudin, leo ac commodo ultricies, quam turpis luctus turpis, eget tincidunt eros turpis vel velit. Duis ultricies tortor vitae porttitor malesuada. Phasellus a elit porttitor, tristique justo eu, fermentum nunc. Curabitur ac efficitur felis. Sed ac diam ac diam fringilla tincidunt et et lectus. Duis sodales vel urna semper placerat. Quisque suscipit scelerisque libero at pretium. Quisque viverra elit et lectus mollis, et facilisis arcu tristique. Nunc tincidunt lobortis diam, vel pellentesque purus imperdiet et. Maecenas est augue, porttitor sed nibh ut, tincidunt porta mauris. Quisque vel consectetur nibh. Mauris eu felis lorem.</p>
+
+        </div>
+      </div>
+    </div>
   </div>
+
 )}
 }
 }
@@ -80,15 +88,21 @@ else {
 class App extends Component {
   render() {
     return (
+
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Wikipedia Viewer</h1>
-          <Random/>
-        </header>
-        <div>
-          <SearchBox/>
+        <div  className = "container py-5 col-md-12 center">
+          <div className = "row ">
+            <header className="App-header">
+              <h1 className="App-title">Wikipedia Viewer</h1>
+              <Random/>
+              </header>
+            <div>
+              <SearchBox/>
+            </div>
+         </div>
         </div>
       </div>
+
     );
   }
 }
